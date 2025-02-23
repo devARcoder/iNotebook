519af1f99166f9dcaae8274bcc29506fcf058868
Abdur Razzaq <abdurrazaq789@gmail.com>
2025-02-23 09:49:15 +0500

    lecture 47 | completed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,35 +1,42 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require("../models/User")
-const { body, validationResult } = require('express-validator');
-
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
 // post request - localhost:5000/api/auth/createuser
-router.post('/createuser',[
-    body('name','Enter a Valid Name').isLength({min: 3}),
-    body('email', 'Enter a Valid Email').isEmail(),
-    body('password', 'Password must be atleast five character').isLength({min: 5}),
-] , async (req, res)=>{
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter a Valid Name").isLength({ min: 3 }),
+    body("email", "Enter a Valid Email").isEmail(),
+    body("password", "Password must be atleast five character").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    let user = await User.findOne({email: req.body.email});
-    if (user){
-      return res.status(400).json({error: "sorry this email already exist"})
-    }
-    user = await User.create({
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "sorry this email already exist" });
+      }
+      user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-      })
-      res.json({"success": "Your email has been successfully added"})
-      
-      // .then(user => res.json(user))
-      // .catch(err=> {console.log(err)
-      //   (res.json({errror: "please enter a unique value to email", message: err.message}))
-      // });
-})
+      });
+      res.json({ success: "Your email has been successfully added" });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("some error ocuure");
+    }
+  }
+);
 
-module.exports = router
+module.exports = router;
