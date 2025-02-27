134f4d1e39795393839ddb1653da3dd2e99905ad
Abdur Razzaq <abdurrazaq789@gmail.com>
2025-02-27 20:47:50 +0500

    lecture 56 | creating navbar and routing | completed

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
